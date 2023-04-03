778205a66b475e16644733fee38dd6b5ef7a1d3a
SergiuCeica <sergiuceica@yahoo.com>
2023-04-03 22:47:33 +0300

    Modificari stil css si Laborator 5

--- js/script.js
+++ js/script.js
@@ -6,17 +6,17 @@ function afiseazaData(){
                 + currentdate.getHours() + ":"  
                 + currentdate.getMinutes() + ":" 
                 + currentdate.getSeconds();
-    document.getElementById("data").innerHTML=currentdate;
-    document.getElementById("url").innerHTML=window.location.href;
+    document.getElementById("data").innerHTML='<i class="material-icons">date_range</i>'+currentdate;
+    document.getElementById("url").innerHTML='<i class="material-icons">folder_open</i>'+window.location.href;
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
       } else {
         document.getElementById("locatie").innerHTML = "Geolocation is not supported by this browser.";
     }
 
-    document.getElementById("browser").innerHTML="Version "+navigator.userAgent;
+    document.getElementById("browser").innerHTML='<i class="material-icons">vpn_lock</i>'+"Version "+navigator.userAgent;
 }
 function showPosition(position) {
-    document.getElementById("locatie").innerHTML = "Latitude: " + position.coords.latitude +
-    "<br>Longitude: " + position.coords.longitude;
+    document.getElementById("locatie").innerHTML = '<i class="material-icons">location_on</i>'+"Latitude: " + position.coords.latitude +
+    "   Longitude: " + position.coords.longitude;
 }
