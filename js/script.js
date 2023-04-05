60516042f2710b4445d2dedc32edfc78ea41dfa6
SergiuCeica <sergiuceica@yahoo.com>
2023-04-05 18:49:34 +0300

    Inceput laborator 6

--- js/script.js
+++ js/script.js
@@ -20,3 +20,60 @@ function showPosition(position) {
     document.getElementById("locatie").innerHTML = '<i class="material-icons">location_on</i>'+"Latitude: " + position.coords.latitude +
     "   Longitude: " + position.coords.longitude;
 }
+
+function addRow(){
+    var table = document.getElementById("tabelJS");
+    var position=document.getElementById("position");
+    var tr= table.insertRow(position.value);
+    var color = document.getElementById("culoareTabel");
+    tr.style.backgroundColor = color.value;
+    var colNr=table.rows[0].cells.length;
+    for(let i =0;i<colNr;i++){
+        var cell=tr.insertCell(i);
+        cell.innerHTML="s";
+    }
+    
+}
+
+function addColumn(){
+    var table = document.getElementById("tabelJS");
+    var position=document.getElementById("position");
+    var td= table.insertColumn(position.value);
+    var color = document.getElementById("culoareTabel");
+    td.style.backgroundColor = color.value;
+    var rowNr=table.rows.length;
+    console.log(rowNr);
+
+}
+
+function drawRect(){
+    var canvas = document.getElementById("canvas");
+    var overlay = document.getElementById("overlay");
+    var ctx = canvas.getContext("2d");
+    var ctxo = overlay.getContext("2d");
+
+    ctx.strokeStyle = "blue";
+    ctx.lineWidth = 3;
+    ctxo.strokeStyle = "blue";
+    ctxo.lineWidth = 3;
+
+    var $canvas = $("#canvas");
+    var canvasOffset = $canvas.offset();
+    var offsetX = canvasOffset.left;
+    var offsetY = canvasOffset.top;
+    var scrollX = $canvas.scrollLeft();
+    var scrollY = $canvas.scrollTop();
+
+    var startX;
+    var startY;
+    
+    if(localStorage.clickCount){
+        if(localStorage.clickCount == 1){
+            
+        }else if(localStorage.clickCount == 2){
+
+        }
+    }else{
+        localStorage.clickCount=1;
+    }
+}
